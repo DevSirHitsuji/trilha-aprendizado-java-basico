4ed3f175b585db4c7f96052fd4479ceaead807b8
DevSirHitsuji <gustavooficial2511@gmail.com>
2022-10-16 15:36:47 -0300

    commit-inicial

---
+++ src/edu/gustavo/tema/AnatomiaDasClasse/anatomiaDasClasses.java
@@ -0,0 +1,7 @@
+package edu.gustavo.tema.AnatomiaDasClasse;
+
+public class anatomiaDasClasses {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/edu/gustavo/tema/tiposMaisVariaveis/tiposMaisVariaveis.java
@@ -0,0 +1,16 @@
+package edu.gustavo.tema.tiposMaisVariaveis;
+
+//Assuntos
+//Numericos inteiros e decimais
+//Tipos logicos
+//caracteres
+//Objetos
+//Diferença entre variavel e constante
+
+public class tiposMaisVariaveis {
+    public static void main(String[] args) {
+        
+    }
+
+    
+}
