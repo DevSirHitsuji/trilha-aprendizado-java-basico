d72691f831b0b19e074d331a3e53b0c1693fc1e8
DevSirHitsuji <gustavooficial2511@gmail.com>
2022-10-17 00:48:55 -0300

    tipos e variaveis completo

---
+++ src/edu/gustavo/tema/operadores/operadores.java
@@ -0,0 +1,7 @@
+package edu.gustavo.tema.operadores;
+
+public class operadores {
+    public static void main(String[] args) {
+        
+    }
+}
--- src/edu/gustavo/tema/tiposMaisVariaveis/tiposMaisVariaveis.java
+++ src/edu/gustavo/tema/tiposMaisVariaveis/tiposMaisVariaveis.java
@@ -9,8 +9,17 @@ package edu.gustavo.tema.tiposMaisVariaveis;
 
 public class tiposMaisVariaveis {
     public static void main(String[] args) {
-        
+        //tipos primitivos
+        byte bite = 127;
+        short shorT = 32000;
+        int inteiro = 1203123013;
+        long longo = 1023941299123412429L;
+        float flutuante = 3.14F;
+        double duplo = 23.0123;
+
+        //constantes
+        final int INTEIRO = 2312;
     }
 
-    
+
 }
