42c3e2a966305ee0aca338494f77985ff6ff78d6
DevSirHitsuji <gustavooficial2511@gmail.com>
2022-10-19 13:14:17 -0300

    terminal e argumento completo. JavaBasico completo

---
+++ src/edu/gustavo/tema/terminalEArgumentos/MinhaClasse.java
@@ -0,0 +1,7 @@
+package edu.gustavo.tema.terminalEArgumentos;
+
+public class MinhaClasse {
+    public static void main(String[] args) {
+        System.out.println("olá, Esta classe foi executada pelo cmd");
+    }
+}
